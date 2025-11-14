1aa6f22c686f88bb5cf47e974f3b733b8b0e10c9
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2025-11-14 23:27:25 +0530

    cors error

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import os
 app = FastAPI(title="RAG FastAPI SQLite FTS with Groq" )
 app.include_router(api_router, prefix="/api")
 
-origins = os.environ.get('CORS_ORIGINS', '*')
+origins = os.environ.get('CORS_ORIGINS', '*',"https://superllm.vercel.app")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[origins] if origins != '*' else ['*'],
